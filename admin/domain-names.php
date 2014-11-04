bd661df7910647d178c1e4c492ee4c94cd1faac8
Remco Tolsma <info@remcotolsma.nl>
2014-11-04 10:46:50 +0100

    Removed white space at end of lines.

--- admin/domain-names.php
+++ admin/domain-names.php
@@ -3,8 +3,8 @@
 
 	<h2><?php echo get_admin_page_title(); ?></h2>
 
-	<?php 
-	
+	<?php
+
 	global $post;
 
 	$query = new WP_Query( array(
@@ -38,11 +38,11 @@
 				<th scope="col"><?php _e( 'Title', 'pronamic_domain_mapping' ); ?></th>
 			</tr>
 		</thead>
-		
+
 		<tbody>
-		
+
 			<?php while ( $query->have_posts() ) : $query->the_post(); ?>
-	
+
 				<tr>
 					<td>
 						<strong>
@@ -53,9 +53,9 @@
 
 						<div class="row-actions">
 							<span class="edit">
-								<a href="<?php echo get_edit_post_link( $post->ID ); ?>"><?php _e( 'Edit', 'pronamic_domain_mapping' ); ?></a> | 
+								<a href="<?php echo get_edit_post_link( $post->ID ); ?>"><?php _e( 'Edit', 'pronamic_domain_mapping' ); ?></a> |
 							</span>
-							
+
 							<span class="view">
 								<a href="<?php the_permalink(); ?>"><?php _e( 'View', 'pronamic_domain_mapping' ); ?></a>
 							</span>
@@ -67,15 +67,15 @@
 						</a>
 					</td>
 				</tr>
-			
+
 			<?php endwhile; ?>
 
 		</tbody>
 	</table>
-	
-	<?php 
-	
+
+	<?php
+
 	wp_reset_postdata();
-	
+
 	?>
 </div>
