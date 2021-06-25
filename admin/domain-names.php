6a0eb96938a3ebe60694400dcd43968801964c06
Reüel van der Steege <reuel@pronamic.nl>
2021-06-25 13:08:28 +0200

    Getting ready for version 2.0.0 with code cleanup.

--- admin/domain-names.php
+++ admin/domain-names.php
@@ -1,29 +1,39 @@
 <?php
+/**
+ * Domain names
+ *
+ * @author    Pronamic <info@pronamic.eu>
+ * @copyright 2005-2021 Pronamic
+ * @license   GPL-3.0-or-later
+ * @package   Pronamic\WordPress\DomainMapping
+ */
 
 global $post;
 
-$query = new WP_Query( array(
-	'post_type'  => 'any',
-	'nopaging'   => true,
-	'meta_query' => array(
-		array(
-			'key'     => '_pronamic_domain_mapping_host',
-			'compare' => 'EXISTS',
+$query = new \WP_Query(
+	array(
+		'post_type'  => 'any',
+		'nopaging'   => true,
+		'meta_query' => array(
+			array(
+				'key'     => '_pronamic_domain_mapping_host',
+				'compare' => 'EXISTS',
+			),
 		),
-	),
-	'order'      => 'ASC',
-	'orderby'    => 'meta_value',
-	'meta_key'   => '_pronamic_domain_mapping_host',
-) );
+		'order'      => 'ASC',
+		'orderby'    => 'meta_value',
+		'meta_key'   => '_pronamic_domain_mapping_host',
+	)
+);
 
 ?>
 <div class="wrap">
-	<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+	<h1><?php echo \esc_html( \get_admin_page_title() ); ?></h1>
 
 	<ul class="subsubsub">
 		<li class="all">
 			<a class="current" href="">
-				<?php esc_html_e( 'All', 'pronamic_domain_mapping' ); ?> <span class="count">(<?php echo esc_html( $query->found_posts ); ?>)</span>
+				<?php \esc_html_e( 'All', 'pronamic_domain_mapping' ); ?> <span class="count">(<?php echo \esc_html( $query->found_posts ); ?>)</span>
 			</a>
 		</li>
 	</ul>
@@ -31,36 +41,42 @@ $query = new WP_Query( array(
 	<table cellspacing="0" class="wp-list-table widefat fixed posts">
 		<thead>
 			<tr>
-				<th scope="col"><?php esc_html_e( 'Domain Name', 'pronamic_domain_mapping' ); ?></th>
-				<th scope="col"><?php esc_html_e( 'Title', 'pronamic_domain_mapping' ); ?></th>
+				<th scope="col"><?php \esc_html_e( 'Domain Name', 'pronamic_domain_mapping' ); ?></th>
+				<th scope="col"><?php \esc_html_e( 'Title', 'pronamic_domain_mapping' ); ?></th>
 			</tr>
 		</thead>
 
 		<tbody>
 
-			<?php while ( $query->have_posts() ) : $query->the_post(); ?>
+			<?php
+
+			while ( $query->have_posts() ) :
+
+				$query->the_post();
+
+				?>
 
 				<tr>
 					<td>
 						<strong>
-							<a href="<?php the_permalink(); ?>">
-								<?php echo esc_html( get_post_meta( $post->ID, '_pronamic_domain_mapping_host', true ) ); ?>
+							<a href="<?php \the_permalink(); ?>">
+								<?php echo \esc_html( \get_post_meta( $post->ID, '_pronamic_domain_mapping_host', true ) ); ?>
 							</a>
 						</strong>
 
 						<div class="row-actions">
 							<span class="edit">
-								<a href="<?php echo esc_attr( get_edit_post_link( $post->ID ) ); ?>"><?php esc_html_e( 'Edit', 'pronamic_domain_mapping' ); ?></a> |
+								<a href="<?php echo \esc_attr( \get_edit_post_link( $post->ID ) ); ?>"><?php \esc_html_e( 'Edit', 'pronamic_domain_mapping' ); ?></a> |
 							</span>
 
 							<span class="view">
-								<a href="<?php the_permalink(); ?>"><?php esc_html_e( 'View', 'pronamic_domain_mapping' ); ?></a>
+								<a href="<?php \the_permalink(); ?>"><?php \esc_html_e( 'View', 'pronamic_domain_mapping' ); ?></a>
 							</span>
 						</div>
 					</td>
 					<td>
-						<a href="<?php the_permalink(); ?>">
-							<?php the_title(); ?>
+						<a href="<?php \the_permalink(); ?>">
+							<?php \the_title(); ?>
 						</a>
 					</td>
 				</tr>
@@ -70,9 +86,5 @@ $query = new WP_Query( array(
 		</tbody>
 	</table>
 
-	<?php
-
-	wp_reset_postdata();
-
-	?>
+	<?php \wp_reset_postdata(); ?>
 </div>
--- includes/version.php
+++ includes/version.php
@@ -1,18 +1,27 @@
 <?php
 /**
-* The Pronamic Domain Mapping version string
-*
-* @global string $pronamic_domain_mapping_version
-*/
-global $pronamic_domain_mapping_version;
-
-$pronamic_domain_mapping_version = '1.0.1';
+ * Version
+ *
+ * @author    Pronamic <info@pronamic.eu>
+ * @copyright 2005-2021 Pronamic
+ * @license   GPL-3.0-or-later
+ * @package   Pronamic\WordPress\DomainMapping
+ */
 
 /**
-* Holds the Pronamic Domain Mapping DB revision, increments when changes are made to the Pronamic Domain Mapping DB schema.
-*
-* @global int $pronamic_domain_mapping_db_version
-*/
+ * The Pronamic Domain Mapping version string
+ *
+ * @global string $pronamic_domain_mapping_version
+ */
+global $pronamic_domain_mapping_version;
+
+$pronamic_domain_mapping_version = '2.0.0';
+
+/**
+ * Holds the Pronamic Domain Mapping DB revision, increments when changes are made to the Pronamic Domain Mapping DB schema.
+ *
+ * @global int $pronamic_domain_mapping_db_version
+ */
 global $pronamic_domain_mapping_db_version;
 
 $pronamic_domain_mapping_db_version = '101';
